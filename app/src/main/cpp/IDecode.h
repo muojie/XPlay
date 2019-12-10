5a90baad7908d796bb1ec8b3f6eed53eafe7a145
muojie <htc.guo@gmail.com>
2019-12-10 19:11:00 +0800

    封装FFDecode解码的Send和Recv接口

--- app/src/main/cpp/IDecode.h
+++ app/src/main/cpp/IDecode.h
@@ -41,6 +41,12 @@ public:
     //打开解码器
     virtual bool Open(XParameter para) = 0;
 
+    //future模型 发送数据到线程解码
+    virtual bool SendPacket(XData pkt) = 0;
+
+    //从线程中获取解码结果
+    virtual XData RecvFrame() = 0;
+
 };
 
 
