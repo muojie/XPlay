312db3cfca7eda8eb982eb43782184d2e74a0a3b
muojie <htc.guo@gmail.com>
2019-12-11 15:47:47 +0800

    申请读取sdcard权限

--- app/src/main/java/xplay/xplay/MainActivity.java
+++ app/src/main/java/xplay/xplay/MainActivity.java
@@ -26,6 +26,9 @@
 
 package xplay.xplay;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -37,10 +40,33 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary( "native-lib" );
     }
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE" };
+
+
+    public static void verifyStoragePermissions(Activity activity) {
+
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        verifyStoragePermissions(this);
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById( R.id.sample_text );
