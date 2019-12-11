6d2dfc0e3ad9d3c00048ec3b26359635bc1345f2
muojie <htc.guo@gmail.com>
2019-12-11 19:47:36 +0800

    SLAudioPlay音频播放完成并测试对MP4文件音视频播放

--- app/src/main/cpp/IAudioPlay.cpp
+++ app/src/main/cpp/IAudioPlay.cpp
@@ -30,7 +30,25 @@
 
 #include "IAudioPlay.h"
 #include "XLog.h"
+XData IAudioPlay::GetData()
+{
+    XData d;
 
+    while(!isExit)
+    {
+        framesMutex.lock();
+        if(!frames.empty())
+        {
+            d = frames.front();
+            frames.pop_front();
+            framesMutex.unlock();
+            return d;
+        }
+        framesMutex.unlock();
+        XSleep(1);
+    }
+    return d;
+}
 void IAudioPlay::Update(XData data)
 {
     XLOGE("IAudioPlay::Update %d",data.size);
