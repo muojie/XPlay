1b498ccf08c80aec3c5df266886b5ede94ae9896
muojie <htc.guo@gmail.com>
2019-12-11 19:45:54 +0800

    IAudioPlay的Update将音频压入缓冲队列并测试

--- app/src/main/cpp/IAudioPlay.cpp
+++ app/src/main/cpp/IAudioPlay.cpp
@@ -29,7 +29,24 @@
 //
 
 #include "IAudioPlay.h"
+#include "XLog.h"
+
 void IAudioPlay::Update(XData data)
 {
+    XLOGE("IAudioPlay::Update %d",data.size);
     //压入缓冲队列
+    if(data.size<=0|| !data.data) return;
+    while(!isExit)
+    {
+        framesMutex.lock();
+        if(frames.size() > maxFrame)
+        {
+            framesMutex.unlock();
+            XSleep(1);
+            continue;
+        }
+        frames.push_back(data);
+        framesMutex.unlock();
+        break;
+    }
 }
--- app/src/main/cpp/IAudioPlay.h
+++ app/src/main/cpp/IAudioPlay.h
@@ -32,15 +32,21 @@
 #define XPLAY_IAUDIOPLAY_H
 
 
+#include <list>
 #include "IObserver.h"
 #include "XParameter.h"
 
 class IAudioPlay: public IObserver
 {
 public:
+    //缓冲满后阻塞
     virtual void Update(XData data);
     virtual bool StartPlay(XParameter out) = 0;
-
+    //最大缓冲
+    int maxFrame = 100;
+protected:
+    std::list <XData> frames;
+    std::mutex framesMutex;
 };
 
 
