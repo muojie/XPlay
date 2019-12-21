7f4dd27485d8afca8b6f8ba5382b55cc8c99add1
muojie <htc.guo@gmail.com>
2019-12-21 14:49:25 +0800

    在835cpu的手机中测试并修正了音频播放Close的bug

--- app/src/main/cpp/SLAudioPlay.cpp
+++ app/src/main/cpp/SLAudioPlay.cpp
@@ -82,7 +82,8 @@ void SLAudioPlay::PlayCall(void *bufq)
         return;
     memcpy(buf,d.data,d.size);
     mux.lock();
-    (*bf)->Enqueue(bf,buf,d.size);
+    if(pcmQue && (*pcmQue))
+        (*pcmQue)->Enqueue(pcmQue,buf,d.size);
     mux.unlock();
     d.Drop();
 
@@ -130,6 +131,12 @@ void SLAudioPlay::Close()
         (*engineSL)->Destroy(engineSL);
     }
 
+    engineSL = NULL;
+    eng = NULL;
+    mix = NULL;
+    player = NULL;
+    iplayer = NULL;
+    pcmQue = NULL;
     mux.unlock();
 }
 
